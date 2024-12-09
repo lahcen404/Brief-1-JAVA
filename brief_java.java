fe79458e6c722223265aa84a1de26e892e026411
lahcen404 <lahcen.maskour2003@gmail.com>
2024-12-09 16:07:47 +0100

    Brief 1 Java

---
+++ brief_java.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class brief_java {
+
+    public static void main(String[] args) {
+
+        
+
+
+    }
+        int Addition (int a, int b) {
+            return a + b;
+    }
+    int Subtraction (int a, int b) {
+        return a - b;
+    }
+    int Multiplication (int a, int b) {
+        return a * b;
+    }
+    int Division (int a, int b) {
+        return a / b;
+    }
+    int Modulo (int a, int b) {
+        return a % b;
+    }
+    
+    
+}
