bce0f6c21fa181572d511ad1812dd45d6b47eddf
lahcen404 <lahcen.maskour2003@gmail.com>
2024-12-10 11:06:05 +0100

    validation devision and Mudelo (devision par Zero not allowed)

--- brief_java.java
+++ brief_java.java
@@ -4,31 +4,69 @@ public class brief_java {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Entre  1st number");
-        int a = sc.nextInt();
+System.out.println("How many operation you want to do");
+int NOp= sc.nextInt();
 
-        System.out.println("Entre  2nd number");
-        int b = sc.nextInt();
+        for (int i=0;i<NOp;i++) {
 
-        System.out.println("Entre symbol");
-        String symbol = sc.nextLine();
-        
+            System.out.println("Entre  1st number");
+            int a = sc.nextInt();
 
+            System.out.println("Entre  2nd number");
+            int b = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Entre symbol  (+ | - | * | / | %)");
+            String symbol = sc.nextLine();
 
+            switch (symbol) {
+                case "+":
+                    System.out.println("L'addition de " + a + " + " + b + " est : " + Addition(a, b));
+                    break;
+                case "-":
+                    System.out.println("La Substraction de " + a + " - " + b + " est : " + Subtraction(a, b));
+                    break;
+                case "*":
+                    System.out.println("La Multiplication de " + a + " * " + b + " est : " + Multiplication(a, b));
+                    break;
+                case "/":
+                    if (b!=0) {
+                        System.out.println("La Division de " + a + " / " + b + " est : " + Division(a, b));
+                    }
+                    else {
+                        System.out.println("Erreur !! Devision par 0 ");
+                    }
+                    break;
+                case "%":
+                    if (b!=0) {
+                        System.out.println("Reste de " + a + " / " + b + " est : " + Modulo(a, b));
+                    }
+                    else {
+                        System.out.println("Erreur !! Devision par 0 ");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid symbol");
+                    break;
+            }
+
+        }
+                    }
+      static int Addition (int a, int b) {
+
+        return a + b;
     }
-        int Addition (int a, int b) {
-            return a + b;
-    }
-    int Subtraction (int a, int b) {
+    static int Subtraction (int a, int b) {
+
         return a - b;
     }
-    int Multiplication (int a, int b) {
+    static int Multiplication (int a, int b) {
+
         return a * b;
     }
-    int Division (int a, int b) {
+    static int Division (int a, int b) {
         return a / b;
     }
-    int Modulo (int a, int b) {
+    static int Modulo (int a, int b) {
         return a % b;
     }
     
